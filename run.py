0dcb1c6bbcb9a44ebfb7e3f039af104d32e2c0b9
Damian Jacob <damianm.jacob@yahoo.com>
2022-01-26 16:07:21 +0000

    Deployment to Heroku

---
+++ run.py
@@ -0,0 +1,60 @@
+import json
+import os
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
+@app.route("/")
+def index():
+    # whatever is returned is what gets displayed in the web browser.
+    # We could just return some html without the render_template function.
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
+
+
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    # Below, the first 'member' is the variable name being passed through into our html file.
+    # The second 'member' is the member object we created above
+    return render_template("member.html", member=member)
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    # flask usually use the get method, the only part that uses the POST method
+    # is this form.  
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
+    return render_template("contact.html", page_title="Contact")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html", page_title="Careers")
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
+
+        
